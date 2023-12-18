9465192f1809411bca8ecb1c6992d3dadf174efb
Khai <quangkhai912016@gmail.com>
2023-12-18 11:24:53 +0700

    Add project files.

---
+++ BanHang/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanHang.Models;
+
+namespace BanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            CnCstoreEntities dBContext = new CnCstoreEntities();
+            var Listproducts = dBContext.Product_Category;
+            return View(Listproducts);
+        }
+    }
+}
---
+++ BanHang/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using BanHang.Models;
+
+namespace BanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(int categoryId = 1)
+        {
+            CnCstoreEntities dBContext = new CnCstoreEntities();
+            List<Product> Listproducts = dBContext.Products
+                                        .Include(cat => cat.Category)
+                                        .Where(pro => pro.CatId == categoryId).ToList();
+            return View(Listproducts);
+        }
+
+        public ActionResult Details(int id)
+        {
+            CnCstoreEntities dBContext = new CnCstoreEntities();
+            Product product = dBContext.Products.Include(cat => cat.Category).FirstOrDefault(x => x.ProId == id);
+            return View(product);
+        }
+
+    }
+}
